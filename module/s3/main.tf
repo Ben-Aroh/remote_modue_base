1c6796e4b037f4ed0398603b581295882d1cc3a9
Ben-Aroh <bennetharoh@gmail.com>
2024-01-10 09:06:56 +0000

    Add main files

---
+++ module/s3/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled =  true
+  }
+}
